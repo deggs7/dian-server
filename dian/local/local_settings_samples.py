d982ea81370986c03c77e87b47bbbfdc793a6600
david <deggs.k@gmail.com>
2015-01-06 12:24:09 +0800

    fix local_settings_samples.py

--- dian/local/local_settings_samples.py
+++ dian/local/local_settings_samples.py
@@ -1,5 +1,5 @@
 """
-local settings, will overwrite bangx2/settings
+local settings, will overwrite dian/settings
 """
 
 
@@ -9,8 +9,8 @@ local settings, will overwrite bangx2/settings
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'bangx2',                      # Or path to database file if using sqlite3.
-#         'USER': 'bangx2',                      # Not used with sqlite3.
+#         'NAME': 'dian',                      # Or path to database file if using sqlite3.
+#         'USER': 'dian',                      # Not used with sqlite3.
 #         'PASSWORD': '123456',                  # Not used with sqlite3.
 #         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
 #         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -22,26 +22,27 @@ local settings, will overwrite bangx2/settings
 
 MONGO_HOST = '127.0.0.1'
 MONGO_PORT = 27017
-MONGO_DB = 'bangx2'
+MONGO_DB = 'dian'
 
 
 # md5 seed
 
-MD5_SEED = 'bangx2.com'
+MD5_SEED = 'dk26.com'
 
 
 # django-cors-headers settings, 
 
 CORS_ORIGIN_WHITELIST = (
     '127.0.0.1:9000',
+    'localhost:9000',
 )
 
 
 # qiniu settings
 
-import qiniu.conf
-qiniu.conf.ACCESS_KEY = "<YOUR_APP_ACCESS_KEY>"
-qiniu.conf.SECRET_KEY = "<YOUR_APP_SECRET_KEY>"
-
-QINIU_UPLOAD_PUBLIC = "bang"
-QINIU_UPLOAD_PRIVATE = "bang-security"
+# import qiniu.conf
+# qiniu.conf.ACCESS_KEY = "<YOUR_APP_ACCESS_KEY>"
+# qiniu.conf.SECRET_KEY = "<YOUR_APP_SECRET_KEY>"
+# 
+# QINIU_UPLOAD_PUBLIC = "bang"
+# QINIU_UPLOAD_PRIVATE = "bang-security"
