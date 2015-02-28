ce738f0da1137030613448464f86d47a7564ac84
david <deggs.k@gmail.com>
2015-02-28 23:57:40 +0800

    add admin for table_type entity

--- dian/restaurant/admin.py
+++ dian/restaurant/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Restaurant
+from .models import TableType
 
 # Register your models here.
 admin.site.register(Restaurant)
+admin.site.register(TableType)
