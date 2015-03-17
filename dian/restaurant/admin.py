c5c011eb5e8eae780531c9895951f1bfd70dc695
david <deggs.k@gmail.com>
2015-03-17 23:39:31 +0800

    register some model to admin

--- dian/restaurant/admin.py
+++ dian/restaurant/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Restaurant
 from .models import TableType
+from .models import Strategy
 
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(TableType)
+admin.site.register(Strategy)
