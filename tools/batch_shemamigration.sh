63d83525fbd5cfa3fe02a52db36dca68594316d0
david <deggs.k@gmail.com>
2015-06-03 15:31:56 +0800

    add tool for batch schemamigration

---
+++ tools/batch_shemamigration.sh
@@ -0,0 +1,5 @@
+python dian/manage.py schemamigration --auto account
+python dian/manage.py schemamigration --auto menu
+python dian/manage.py schemamigration --auto registration
+python dian/manage.py schemamigration --auto restaurant
+python dian/manage.py schemamigration --auto table
