f2ac9a082a8b8c771f4d322dc7c1e2413b38014e
david <deggs.k@gmail.com>
2015-06-26 16:28:56 +0800

    add batch script for init schemamigration

---
+++ tools/batch_init_shemamigration.sh
@@ -0,0 +1,6 @@
+python dian/manage.py schemamigration --initial account
+python dian/manage.py schemamigration --initial menu
+python dian/manage.py schemamigration --initial registration
+python dian/manage.py schemamigration --initial restaurant
+python dian/manage.py schemamigration --initial table
+python dian/manage.py schemamigration --initial trade
