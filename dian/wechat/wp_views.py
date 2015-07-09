fdf6de604b3555f7d010a4deb04ac12f513fc376
david <deggs.k@gmail.com>
2015-07-09 21:23:34 +0800

    get parameter from request.DATA when $http.post()

--- dian/wechat/wp_views.py
+++ dian/wechat/wp_views.py
@@ -50,7 +50,7 @@ def get_jsapi_signature(request):
         - code: 400
           message: Url Error
     """
-    url = request.GET.get('url', None)
+    url = request.DATA.get('url', None)
     if url:
         return Response(_generate_jsapi_signature_body(url))
     else:
