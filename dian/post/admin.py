5f7663a539d496fe9eb229c2a6bbbeb9a7626b2b
Xiao Hanyu <xiaohanyu1988@gmail.com>
2015-07-11 19:26:05 +0800

    Add like table to admin.

--- dian/post/admin.py
+++ dian/post/admin.py
@@ -2,9 +2,10 @@
 
 from django.contrib import admin
 
-from post.models import Image, Tag, Post
+from post.models import Image, Tag, Post, Like
 
 
 admin.site.register(Image)
 admin.site.register(Tag)
+admin.site.register(Like)
 admin.site.register(Post)
