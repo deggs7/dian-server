e65b9bff0792244f40cf2a0d5f0c4358776c0937
Xiao Hanyu <xiaohanyu1988@gmail.com>
2015-07-12 11:02:09 +0800

    Fix typo.

--- dian/post/urls.py
+++ dian/post/urls.py
@@ -4,12 +4,4 @@ from django.conf.urls import patterns
 from django.conf.urls import url
 
 
-urlpatterns = patterns(
-    'post.views',
-
-    url(r'^list-post/$', 'list_post'),
-    url(r'^create-post/$', 'create_post'),
-    url(r'^get-post/(?P<pk>[0-9]+)/$', 'get_post'),
-    url(r'^update-post/(?P<pk>[0-9]+)/$', 'update_post'),
-    url(r'^delete-post/(?P<pk>[0-9]+)/$', 'delete_post'),
-)
+urlpatterns = patterns('post.views')
--- dian/post/wp_urls.py
+++ dian/post/wp_urls.py
@@ -5,11 +5,10 @@ from django.conf.urls import url
 
 
 urlpatterns = patterns(
-    'post.views',
+    'post.wp_views',
 
     url(r'^list-post/$', 'list_post'),
     url(r'^create-post/$', 'create_post'),
     url(r'^get-post/(?P<pk>[0-9]+)/$', 'get_post'),
     url(r'^update-post/(?P<pk>[0-9]+)/$', 'update_post'),
-    url(r'^delete-post/(?P<pk>[0-9]+)/$', 'delete_post'),
 )
