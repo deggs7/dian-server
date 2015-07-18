58c1cf3169617444f1ff13d577be39c965c4e70c
david <deggs.k@gmail.com>
2015-07-18 21:56:27 +0800

    temporary commit

--- dian/post/wp_urls.py
+++ dian/post/wp_urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns(
     url(r'^list-my-like/(?P<member_id>\w+)/$', 'list_my_like'),
     url(r'^get-overview-of-my-like/$', 'get_overview_of_my_like'),
     url(r'^get-overview-of-my-like/(?P<member_id>\w+)/$', 'get_overview_of_my_like'),
-    url(r'^create-post/$', 'create_post'),
+    # url(r'^create-post/$', 'create_post'),
     url(r'^get-next-post-list/(?P<limit>[0-9]+)/$', 'get_next_post_list'),
     url(r'^like-post/(?P<post_id>[0-9]+)/$', 'like_post'),
     url(r'^like-post/(?P<post_id>[0-9]+)/(?P<member_id>\w+)/$', 'like_post'),
