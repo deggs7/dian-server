ea7aa1aea69ebc3700b8830540085533d038730a
david <deggs.k@gmail.com>
2016-01-17 23:25:42 +0800

    add cache with redis

--- dian/local/local_settings_sample.py
+++ dian/local/local_settings_sample.py
@@ -35,6 +35,16 @@ BROKER_URL = 'amqp://dian:123456@localhost:5672//dianvhost'
 
 # Cache
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS":
+            "django_redis.client.DefaultClient",
+        }
+    }
+}
 # CACHES = {
 #     'default': {
 #         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
